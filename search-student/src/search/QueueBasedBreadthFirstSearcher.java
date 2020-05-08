fed927e9833eaab45817d2ec60618eb8dcc642d4
Nafis Rahman <42381892+nafisrahman@users.noreply.github.com>
2020-05-08 19:04:47 -0400

    Update QueueBasedBreadthFirstSearcher.java

--- search-student/src/search/QueueBasedBreadthFirstSearcher.java
+++ search-student/src/search/QueueBasedBreadthFirstSearcher.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.HashMap;
 
 /**
  * An implementation of a Searcher that performs an iterative search,
@@ -27,11 +28,27 @@ public class QueueBasedBreadthFirstSearcher<T> extends Searcher<T> {
     // TODO
     Queue<T> frontier = new LinkedList<>();
     frontier.add(searchProblem.getInitialState());
+    HashMap<T, T> predecessor = new HashMap<>();
+    predecessor.put(searchProblem.getInitialState(), null);
     List<T> path = new ArrayList<>();
     while (!frontier.isEmpty()) {
       T current = frontier.remove();
-      
+      for (T next : searchProblem.getSuccessors(current)) {
+        if (!predecessor.containsKey(next)) {
+          frontier.add(next);
+          predecessor.put(next, current);
+        }
+      }
+      if (searchProblem.isGoal(current)) {
+        path.add(current);
+        T previous = predecessor.get(current);
+        while (previous != null) {
+          path.add(0, previous);
+          previous = predecessor.get(previous);
+        }
+        break;
+      }
     }
-    return null;
+    return path;
   }
 }
