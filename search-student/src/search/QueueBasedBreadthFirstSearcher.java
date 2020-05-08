9318cad01711cc27fb9376b7deba076931756447
Nafis Rahman <42381892+nafisrahman@users.noreply.github.com>
2020-05-08 18:53:09 -0400

    Add files via upload

---
+++ search-student/src/search/QueueBasedBreadthFirstSearcher.java
@@ -0,0 +1,37 @@
+package search;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * An implementation of a Searcher that performs an iterative search,
+ * storing the list of next states in a Queue. This results in a
+ * breadth-first search.
+ * 
+ */
+public class QueueBasedBreadthFirstSearcher<T> extends Searcher<T> {
+
+  /**
+   * QueueBasedBreadthFirstSearcher.
+   * @param searchProblem : search problem
+   */
+  public QueueBasedBreadthFirstSearcher(SearchProblem<T> searchProblem) {
+    super(searchProblem);
+  }
+
+  @Override
+  public List<T> findSolution() {
+    // TODO
+    Queue<T> frontier = new LinkedList<>();
+    frontier.add(searchProblem.getInitialState());
+    List<T> path = new ArrayList<>();
+    while (!frontier.isEmpty()) {
+      T current = frontier.remove();
+      
+    }
+    return null;
+  }
+}
---
+++ search-student/src/search/StackBasedDepthFirstSearcher.java
@@ -0,0 +1,30 @@
+package search;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * An implementation of a Searcher that performs an iterative search,
+ * storing the list of next states in a Stack. This results in a
+ * depth-first search.
+ * 
+ */
+public class StackBasedDepthFirstSearcher<T> extends Searcher<T> {
+
+  /**
+   * StackBasedDepthFirstSearcher.
+   * @param searchProblem : search problem
+   */
+  public StackBasedDepthFirstSearcher(SearchProblem<T> searchProblem) {
+    super(searchProblem);
+  }
+
+  @Override
+  public List<T> findSolution() {
+    // TODO
+    
+    return null;
+  }
+}
